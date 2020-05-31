b642e4c75a94c6a079c188453aebe29e1f9f803c
hzl1998 <1445419939@qq.com>
2020-05-31 20:06:19 +0800

    。。。

--- index.js
+++ index.js
@@ -152,7 +152,7 @@ export class ImageExtend {
             if (xhr.readyState === 4) {
                 if (xhr.status === 200) {
                     //success
-                    let res = JSON.parse(xhr.responseText)
+                    let res = typeof xhr.responseText=='string'?JSON.parse(xhr.responseText):xhr.responseText
                     self.imgURL = config.response(res)
                     QuillWatch.active.uploadSuccess()
                     self.insertImg()
